3a921650c2d30a18f5931cc86b8260dd6fc8e56a
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-07-21 16:29:09 +0530

    delete old glfw file

--- main.c
+++ main.c
@@ -208,7 +208,6 @@ int main() {
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow *window = glfwCreateWindow(winw, winh, "Todo", NULL, NULL);
   if (!window) {
