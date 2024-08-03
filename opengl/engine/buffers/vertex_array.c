d1ca8a7c377608c10d45b1a9c1ff6e3f66e3d0fe
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2024-08-03 16:14:31 +0530

    abstraction vbo, vao, ibo (glcall error fix)

--- opengl/engine/buffers/vertex_array.c
+++ opengl/engine/buffers/vertex_array.c
@@ -2,13 +2,13 @@
 
 VertexArray *vertex_array_create() {
   VertexArray *vao = (VertexArray *)malloc(sizeof(VertexArray));
-  glGenVertexArrays(1, &vao->rendererId);
+  GLCall(glGenVertexArrays(1, &vao->rendererId));
   vertex_array_bind(vao);
   return vao;
 }
 
 void vertex_array_destroy(VertexArray *vao) {
-  glDeleteVertexArrays(1, &vao->rendererId);
+  GLCall(glDeleteVertexArrays(1, &vao->rendererId));
 }
 
 void vertex_array_add_buffer(VertexArray *vao, VertexBuffer *vb,
@@ -19,13 +19,16 @@ void vertex_array_add_buffer(VertexArray *vao, VertexBuffer *vb,
   unsigned int offset = 0;
   for (unsigned int i = 0; i < layout->elementCount; i++) {
     VertexBufferElement element = layout->elements[i];
-    glEnableVertexAttribArray(i);
-    glVertexAttribPointer(i, element.count, element.type, element.normalized,
-                          layout->stride, (const void *)(uintptr_t)offset);
+    GLCall(glEnableVertexAttribArray(i));
+    GLCall(glVertexAttribPointer(i, element.count, element.type,
+                                 element.normalized, layout->stride,
+                                 (const void *)(uintptr_t)offset));
     offset += element.count * get_size_of_type(element.type);
   }
 }
 
-void vertex_array_bind(VertexArray *vao) { glBindVertexArray(vao->rendererId); }
+void vertex_array_bind(VertexArray *vao) {
+  GLCall(glBindVertexArray(vao->rendererId));
+}
 
-void vertex_array_unbind(VertexArray *vao) { glBindVertexArray(0); }
+void vertex_array_unbind(VertexArray *vao) { GLCall(glBindVertexArray(0)); }
